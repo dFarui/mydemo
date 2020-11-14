6e85e15705f05bff247b1feb1613e8728e261ca2
dFarui <dfaruiy@gmail.com>
2020-11-15 03:08:43 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
-###猫眼电影前100排名###
+# 猫眼电影前100排名
 
 import requests
 from requests.exceptions import RequestException
@@ -10,17 +9,19 @@ import re
 import json
 import time
 
+
 def get_one_page(url):
     headers = {
-        'User-Agent':'Mozilla/5.0(Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0(Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/65.0.3325.162 Safari/537.36'
     }
     try:
-        response = requests.get(url,headers=headers)
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.text
         return None
     except RequestException:
         return None
+
 
 def parse_one_page(html):
     pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
@@ -29,13 +30,14 @@ def parse_one_page(html):
     items = re.findall(pattern, html)
     for item in items:
         yield {
-            'index':item[0],
-            'image':item[1],
-            'title':item[2].strip(),
-            'actor':item[3].strip()[3:] if len(item[3]) > 3 else '',
-            'time':item[4].strip()[5:] if len(item[4]) > 5 else '',
-            'score':item[5].strip()+item[6].strip()
+            'index': item[0],
+            'image': item[1],
+            'title': item[2].strip(),
+            'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
+            'time': item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score': item[5].strip()+item[6].strip()
         }
+
 
 def write_to_file(content):
     with open('result.txt', 'a', encoding='utf-8') as f:
